ac3903aa2873877f2eb63f59bae9a1b938c2dcbe
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-01-30 11:48:43 +0100

    Minor bug fixed

--- Linux/orgit-1.0.0.sh
+++ Linux/orgit-1.0.0.sh
@@ -7,19 +7,19 @@
 
 # Variables
 GIT="https://github.com/gpietrafesavieitez"
-DIR="$HOME/NetBeansProjects/orgitCloned"
+DIR="$HOME/NetBeansProjects/DAM1"
 
 # Primero borro dir
 rm -rf $DIR
 
 # Luego creo dir
-mkdir -p $DIR/DAM1/COD $DIR/DAM1/COD/Pruebas $DIR/DAM1/PROG $DIR/DAM1/PROG/Boletines $DIR/DAM1/PROG/Pruebas $DIR/DAM1/PROG/Extras
+mkdir -p $DIR/COD $DIR/COD/Pruebas $DIR/PROG $DIR/PROG/Boletines $DIR/PROG/Pruebas $DIR/PROG/Extras
 
 # Clonacion
 echo "Clonando repositorios principales, porfavor espere..."
-git clone -q $GIT/COD_Pruebas.git $DIR/DAM1/COD/Pruebas
-git clone -q $GIT/PROG_Extras.git $DIR/DAM1/PROG/Extras
-git clone -q $GIT/PROG_Pruebas.git $DIR/DAM1/PROG/Pruebas
+git clone -q $GIT/COD_Pruebas.git $DIR/COD/Pruebas
+git clone -q $GIT/PROG_Extras.git $DIR/PROG/Extras
+git clone -q $GIT/PROG_Pruebas.git $DIR/PROG/Pruebas
 echo "Clonacion de repositorios principales finalizada."
 
 # Loop para clonar boletines concretos
@@ -27,7 +27,7 @@ read -p "Introduzca el numero del ultimo boletin creado: " leer
 echo "Clonando boletines, porfavor espere..."
 for (( i = 1; i <= $leer; i++ )) 
 do
-	git clone -q $GIT/PROG_Boletin_$i.git $DIR/DAM1/PROG/Boletines/Boletin_$i
+	git clone -q $GIT/PROG_Boletin_$i.git $DIR/PROG/Boletines/Boletin_$i
 done
 
 echo "Clonacion finalizada, gracias por usarme :D"
